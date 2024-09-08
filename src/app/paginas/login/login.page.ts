6220368f7fa57e0d45ee3819fc761cc836948388
MkOMIN <134329691+MkOMIN@users.noreply.github.com>
2024-09-07 21:13:21 -0400

    commit

--- src/app/paginas/login/login.page.ts
+++ src/app/paginas/login/login.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Route, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
@@ -9,45 +9,49 @@ import { AlertController, ToastController } from '@ionic/angular';
 })
 export class LoginPage implements OnInit {
 
-  nombre:string = ""
-  usuarios:string = ""
-  password:string = ""
+  nombre:string =""
+  usuario :string =""
+  password :string =""
+  isModalOpen = false;
 
-  constructor(public mensaje:ToastController, private route:Router, public alerta:AlertController) { }
+  constructor(public mensaje:ToastController, private route:Router,public alerta:AlertController) { }
 
   async mensajeExito() {
     const toast = await this.mensaje.create({
-      message: 'Inicio de sesion Exitoso ',
+      message: 'Incio de session exitoso ',
       duration: 2000
     });
     toast.present();
   }
-
   async MensajeError() {
     const alert = await this.alerta.create({
       header: 'Error',
-      subHeader: 'Error de inicio de sesion',
-      message: 'El inicio de sesion fallo',
+      subHeader: 'Error de inicio de seson ',
+      message: 'El inicio de sesion fallo ',
       buttons: ['Aceptar']
     });
   
     await alert.present();
   }
 
+  
+
   ingresar(){
-    if(this.usuarios=== "" && this.password=== ""){
-      console.log("No pueden estar los campos vacios")
+    if(this.usuario ==="" && this.password===""){
+      console.log("No pueden estar los campos vacios ")
       this.MensajeError()
     }
     else{
-      console.log("inicio Exitoso")
+      console.log("inicio exitoso ")
       this.mensajeExito()
       this.route.navigate(["/home"])
     }
-    
   }
 
   ngOnInit() {
   }
 
+  setOpen(isOpen: boolean) {
+      this.isModalOpen = isOpen;
+    }
 }
