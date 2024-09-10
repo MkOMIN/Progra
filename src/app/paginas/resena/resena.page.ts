9922d8312bd29a12b46d86adc22f1167544bda4f
MkOMIN <134329691+MkOMIN@users.noreply.github.com>
2024-09-10 11:25:55 -0300

    commit

--- src/app/paginas/resena/resena.page.ts
+++ src/app/paginas/resena/resena.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-resena',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResenaPage implements OnInit {
 
-  constructor() { }
+  constructor(private route:Router) { }
 
   ngOnInit() {
   }
 
+  home(){
+    console.log("Home");
+    this.route.navigate(["/home"]);
+  }
 }
