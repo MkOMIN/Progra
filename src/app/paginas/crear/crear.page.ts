877ed75d5b9414c2416318eaf73d2328e4a727ec
MkOMIN <134329691+MkOMIN@users.noreply.github.com>
2024-09-25 23:16:14 -0300

    empeze con el state de la pagina de crear resena

--- src/app/paginas/crear/crear.page.ts
+++ src/app/paginas/crear/crear.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ColdObservable } from 'rxjs/internal/testing/ColdObservable';
 
 @Component({
   selector: 'app-crear',
@@ -10,9 +11,19 @@ import { Router } from '@angular/router';
 
 export class CrearPage implements OnInit {
 
+  producto: string = "";
+  ingredientes: string = "";
+
+
   constructor(private route:Router) { }
 
   ngOnInit() {
+  }
+
+  enviar(){
+    console.log("Reseña Enviada");
+    localStorage.setItem("producto", this.producto);
+    this.route.navigate(["/home"]);
   }
 
   home(){
