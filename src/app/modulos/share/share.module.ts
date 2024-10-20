03d083b0c4a67052e090bf6aec47ff87ac87d74f
MJGP22 <maur.galdames@duocuc.lc>
2024-10-20 00:02:13 -0300

    Construccion nuevo components

--- src/app/modulos/share/share.module.ts
+++ src/app/modulos/share/share.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { IonicModule } from '@ionic/angular';
+import { FooterComponent } from 'src/app/componentes/footer/footer.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [FooterComponent],
+  exports:[FooterComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    IonicModule
   ]
 })
 export class ShareModule { }
